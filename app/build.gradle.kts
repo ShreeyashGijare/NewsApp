111fae2169c2859a775a62faf0f531de725d1fe0
ShreeyashGijare <shreeyash.gijare.rm@synergygfs.com>
2024-04-28 12:46:11 +0530

    Notifications commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("kotlin-parcelize")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,6 +48,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-messaging-ktx:23.4.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,5 +70,9 @@ dependencies {
 
     //Gson
     implementation("com.google.code.gson:gson:2.5")
+
+    //FireBase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("com.google.firebase:firebase-analytics")
 
 }
---
+++ app/src/main/java/com/example/taskmoengage/ui/activity/SplashActivity.kt
@@ -0,0 +1,63 @@
+package com.example.taskmoengage.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.taskmoengage.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivitySplashBinding
+    private lateinit var requestLauncher: ActivityResultLauncher<String>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        getPermission()
+        askForPermission()
+    }
+
+    private fun moveToNextScreen() {
+        mBinding.ivLogo.scaleX = 1f
+        mBinding.ivLogo.scaleY = 1f
+        performZoomInAndOutAnimation()
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 2000)
+    }
+
+    private fun performZoomInAndOutAnimation() {
+        val zoomInAnimation = ScaleAnimation(
+            1f, 2f,
+            1f, 2f,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        zoomInAnimation.duration = 2500
+
+        mBinding.ivLogo.startAnimation(zoomInAnimation)
+    }
+
+    private fun getPermission() {
+        requestLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                moveToNextScreen()
+            } else {
+                askForPermission()
+            }
+        }
+    }
+
+    private fun askForPermission() {
+        requestLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+    }
+}
